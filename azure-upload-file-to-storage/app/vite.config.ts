51e93a5257880f52951cecdb9184806287a45dbb
josewer01 <josewer01@gmail.com>
2024-05-14 04:48:25 +0200

    cambio

--- azure-upload-file-to-storage/app/vite.config.ts
+++ azure-upload-file-to-storage/app/vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
@@ -6,6 +7,10 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
+      input: {
+        main: resolve('app', 'index.html'),
+        nested: resolve('app', 'nested/add.html'),
+      },
       output: {
         manualChunks: {
           //@azure/storage-blob: ['@azure/storage-blob']
